0df088802b2817ce9049a4b042bfcb608a89417d
Daniel S <dsu@uoregon.edu>
2017-04-13 15:39:19 -0700

    add more getter & setters for private data type

--- Person.java
+++ Person.java
@@ -1,21 +1,32 @@
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Scanner;
-import javax.swing.AbstractButton;
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import java.io.File;
+import java.util.HashSet;
 
 public class Person {
 	private String name;
+	private String lastName;
+	private HashSet<Integer> availablity;
+	private String[] languages;
+	
 	public void set_name(String new_name){
 		name = new_name;
 	}
 	public String get_name(){
 		return name;
 	}
-    
+	public void set_lastName(String new_lastName){
+		lastName = new_lastName;
+	}
+	public String get_lastName(){
+		return lastName;
+	}public void set_availablity(HashSet<Integer> new_availablity){
+		availablity = new_availablity;
+	}
+	public HashSet<Integer> get_availablity(){
+		return availablity;
+	}
+	public void set_languages(String[] new_languages){
+		languages = new_languages;
+	}
+	public String[] get_languages(){
+		return languages;
+	}
 }
