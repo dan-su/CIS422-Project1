73103e5160b792e623ebcb9ae27fc758578c1771
Daniel S <dsu@uoregon.edu>
2017-04-18 16:58:39 -0700

    add comments on team.java & change get_diversity function return string

--- Team.java
+++ Team.java
@@ -1,28 +1,32 @@
 public class Team {
 	private Person[] members;
 	private Integer quality;
-	private Integer diversity;
+	private String diversity;
 	private Person teamCapn;
 	
 	public void set_members(Person[] new_members){
+		//sets all members of the team
 		members = new_members;
 	}
 	public Person[] get_members(){
 		return members;
 	}
 	public void set_quality(Integer new_quality){
+		//sets the guality of the team, which is based off the average of sum of members' availablityMatch
 		quality = new_quality;
 	}
 	public Integer get_quality(){
 		return quality;
 	}
 	public void set_diversity(Integer new_diversity){
+		//sets the diversity aspect of the team 
 		diversity = new_diversity;
 	}
-	public Integer get_diversity(){
+	public String get_diversity(){
 		return diversity;
 	}
 	public void get_teamCapn(Person new_teamCapn){
+		//sets the team "captain", which is the person with the lowest availablityMatch value
 		teamCapn = new_teamCapn;
 	}
 	public Person get_teamCapn(){
